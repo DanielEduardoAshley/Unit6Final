1164745cca7ac84c08821cdc1e413bab612868d1
daniel ashley <danielashley@pursuit.org>
2019-04-17 21:43:05 -0400

    Tested post comments and it works

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -3,6 +3,24 @@ const commentsRouter = express.Router();
 const commentsServices = require('../services/commentsServices');
 
 
+
+// POST new comment
+commentsRouter.post('/', (req, res)=>{
+    const {comment_body, user_id, show_id } = req.body
+    commentsServices.create(comment_body, user_id, show_id)
+    .then(()=>{
+        console.log('Success comment created')
+        res.status(200)
+        res.json(`Success comment created`)
+    })
+    .catch(err=>{
+        console.log('Something went wrong in post comment route', err)
+        res.json('Something went wrong in post comment route')
+    })
+    
+})
+
+
 // GET all comments for specific show_id
 commentsRouter.get('/:comment_id', (req, res)=>{
     const { comment_id } = req.params
@@ -19,20 +37,7 @@ commentsRouter.get('/:comment_id', (req, res)=>{
     
 })
 
-// POST new comment
-commentsRouter.post('/', (req, res)=>{
-    commentsServices.create()
-    .then(()=>{
-        console.log('Success comment created')
-        res.status(200)
-        res.json(`Success comment created`)
-    })
-    .catch(err=>{
-        console.log('Something went wrong in post comment route', err)
-        res.json('Something went wrong in post comment route')
-    })
-    
-})
+
 
 
 
--- backend/services/commentsServices.js
+++ backend/services/commentsServices.js
@@ -3,13 +3,13 @@ const commentsServices = {};
 
 // POST new comment
 commentsServices.create=(comment_body, user_id, show_id)=>{
-    return db.one('INSERT INTO comments (comment_body, user_id, show_id) VALUES ($[comment_body], $[user_id], $[show_id])')
+    return db.none('INSERT INTO comments (comment_body, user_id, show_id) VALUES ($[comment_body], $[user_id], $[show_id])', { comment_body, user_id, show_id })
 };
 
 
 // GET all comments for specific show_id
 commentsServices.read=(show_id)=>{
-    return db.any('SELECT * FROM shows WHERE id=$[show_id]', {show_id})
+    return db.any('SELECT * FROM comments WHERE id=$[show_id]', {show_id})
 };
 
 module.exports = commentsServices
